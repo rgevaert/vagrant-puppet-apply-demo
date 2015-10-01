dcc73affeb0ad43d05e0a607c33334472945f3bd
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2015-10-01 16:36:30 +0200

    compiled squid

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,19 @@
 node default {
-  include squid
+#  include squid
+
+  file {
+    '/var/tmp/squid_3.5.9_amd64.deb':
+      ensure => present,
+      source => 'puppet:///modules/squid/squid_3.5.9_amd64.deb';
+  }
+
+  # fpm -s dir -t deb -n squid -v 3.5.9 /usr/local/squid
+  package {
+    'squid':
+      ensure   => installed,
+      require => File['/var/tmp/squid_3.5.9_amd64.deb'],
+      provider => dpkg,
+      source   => '/var/tmp/squid_3.5.9_amd64.deb';
+  }
+
 }
